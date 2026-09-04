3e2034df0e4bf2a0fa797e11fd8a7d37fdcdef6b
Anna <anna@hypership.tech>
2026-09-04 15:07:11 +0100

    Completed 09 sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if ((a <= 0 || !Number.isInteger(a)) || (b <= 0 || !Number.isInteger(b))){
+        return "ERROR";
+    }
 
+    const start = Math.min(a, b);
+    const end = Math.max(a, b);
+
+    let sum = 0;
+
+    for (let index = start; index <= end ; index++) {
+        sum += index;
+        
+    }
+return sum
 };
 
 // Do not edit below this line
