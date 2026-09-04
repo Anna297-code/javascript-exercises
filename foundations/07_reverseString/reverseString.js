9b6b42aaf17832ca9163f490d4fccc3b883ec068
Anna <anna@hypership.tech>
2026-09-04 13:11:52 +0100

    Completed 07 reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,4 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let char = str.split('');
+    let charReversed = char.reverse();
+    let result = charReversed.join('');
+
+    return result;
+ 
 
 };
 
