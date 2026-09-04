fdeffda5ebc6a01e64a8c94d3049e38573a8ab5f
Anna <anna@hypership.tech>
2026-09-04 16:03:59 +0100

    Completed 11 tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let farenConversion = (num - 32)*(5/9);
+  let roundedValue = Math.round(farenConversion * 10)/10
+  return roundedValue
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let celsiusConversion = (num * (9/5)) + 32;
+  let roundedValue = Math.round(celsiusConversion * 10)/10
+  return roundedValue
 };
 
 // Do not edit below this line
