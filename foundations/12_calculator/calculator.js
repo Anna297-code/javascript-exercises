05e69233d2276bd2cec86cfdd953d25280df5414
Anna <anna@hypership.tech>
2026-09-11 16:33:53 +0100

    finished 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,38 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((total, value) => total + value, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((acc, value) => acc * value, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a === 0 || a === 1) {
+    return 1;
+  }
+
+  let result = a;
+
+  for (let index = a; index > 1; index--) {
+    result *= index - 1;
+  }
+
+  return result;
 };
+
+console.log(factorial(5));
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +41,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
