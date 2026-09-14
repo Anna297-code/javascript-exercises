eafdbb36850314c0d3f08ab9ea963e6a31c46a85
Anna <anna@hypership.tech>
2026-09-14 15:32:32 +0100

    Completed 14 Fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  number = Number(number);
+  if (number < 0) {
+    return "OOPS";
+  }
 
+  if (number === 0) {
+    return 0;
+  }
+
+  let previous = 1;
+  let previousPrevious = 1;
+
+  for (let index = 0; index < number - 2; index++) {
+    let newNumber = previous + previousPrevious;
+    previousPrevious = previous;
+    previous = newNumber;
+  }
+
+  return previous;
 };
 
 // Do not edit below this line
