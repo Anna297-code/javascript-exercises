6ce431e1257667ef95ec3c9d623c49b6edfec637
Anna <anna@hypership.tech>
2026-09-14 12:05:09 +0100

    Completed 13 Palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,32 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let lowercaseString = string.toLowerCase();
 
+  let characters = lowercaseString.split("");
+
+  let filteredCharacters = characters.filter(
+    (character) =>
+      !(
+        character === " " ||
+        character === "," ||
+        character === "." ||
+        character === "!"
+      ),
+  );
+
+  let filteredString = filteredCharacters.join("");
+
+  let reversedCharacters = filteredCharacters.toReversed();
+
+  let reversedString = reversedCharacters.join("");
+
+  if (filteredString === reversedString) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
+palindromes("Racecar!");
 
 // Do not edit below this line
 module.exports = palindromes;
