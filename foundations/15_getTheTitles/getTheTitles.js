cc88c733c672da54b644d85d42f6383f1eff49f8
Anna <anna@hypership.tech>
2026-09-14 16:17:48 +0100

    Completed 15 getTheTitles

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,20 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: "Book",
+    author: "Name",
+  },
+  {
+    title: "Book2",
+    author: "Name2",
+  },
+];
 
+const getTheTitles = function (arr) {
+  const newArr = arr.map((item) => item.title);
+  return newArr;
 };
+
+getTheTitles(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;
